7f526417c053bc59f9556005a80ba5c544de83c8
Max Strübing <mxstrbng@gmail.com>
2025-02-22 22:42:42 +0000

    refactor: build url on backend

--- .nvim.lua
+++ .nvim.lua
@@ -1,14 +1,14 @@
 local autocmd_group = vim.api.nvim_create_augroup("Custom auto-commands", { clear = true })
 
-vim.api.nvim_create_autocmd({ "BufWritePost" }, {
-    pattern = { "*.rs"},
-    desc = "Auto-format rs files after saving",
-    callback = function()
-        local fileName = vim.api.nvim_buf_get_name(0)
-        vim.cmd(":silent !rustfmt --edition 2021 " .. fileName)
-    end,
-    group = autocmd_group,
-})
+-- vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+--     pattern = { "*.rs"},
+--     desc = "Auto-format rs files after saving",
+--     callback = function()
+--         local fileName = vim.api.nvim_buf_get_name(0)
+--         vim.cmd(":silent !rustfmt --edition 2021 " .. fileName)
+--     end,
+--     group = autocmd_group,
+-- })
 
 vim.api.nvim_create_autocmd({ "BufWritePost" }, {
     pattern = { "*.elm"},
