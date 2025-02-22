948b1cf3fbce7730c01ff18010047423386735d4
Max Strübing <mxstrbng@gmail.com>
2025-02-22 00:49:37 +0000

    feat: add cors

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,14 @@ extern crate rocket;
 
 use std::env;
 
-use rocket::fs::{relative, FileServer};
+use rocket::{
+    fairing::{Fairing, Info, Kind},
+    fs::{relative, FileServer},
+    http::Header,
+};
+
+use rocket::{Request, Response};
+
 use serde::Serialize;
 
 use regex::Regex;
@@ -49,10 +56,12 @@ fn rocket() -> _ {
     let rocket_env = env::var("ROCKET_ENV");
     match rocket_env {
         Ok(_) => rocket::build()
-            .mount("/player", routes![player_route])
+            .attach(Cors)
+            .mount("/player", routes![player_route, all_options])
             .mount("/", FileServer::from("/www/public")),
         Err(_) => rocket::build()
-            .mount("/player", routes![player_route])
+            .attach(Cors)
+            .mount("/player", routes![player_route, all_options])
             .mount("/", FileServer::from(relative!("frontend/dist"))),
     }
 }
@@ -66,4 +75,32 @@ fn extract_steam_id(lines: &str) -> Option<String> {
 
     let steam_id = &caps[1];
     Some(steam_id.to_string())
+}
+
+/// Catches all OPTION requests in order to get the CORS related Fairing triggered.
+#[options("/<_..>")]
+fn all_options() {
+    /* Intentionally left empty */
+}
+
+pub struct Cors;
+
+#[rocket::async_trait]
+impl Fairing for Cors {
+    fn info(&self) -> Info {
+        Info {
+            name: "Cross-Origin-Resource-Sharing Fairing",
+            kind: Kind::Response,
+        }
+    }
+
+    async fn on_response<'r>(&self, _request: &'r Request<'_>, response: &mut Response<'r>) {
+        response.set_header(Header::new("Access-Control-Allow-Origin", "*"));
+        response.set_header(Header::new(
+            "Access-Control-Allow-Methods",
+            "POST, PATCH, PUT, DELETE, HEAD, OPTIONS, GET",
+        ));
+        response.set_header(Header::new("Access-Control-Allow-Headers", "*"));
+        response.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
+    }
 }
