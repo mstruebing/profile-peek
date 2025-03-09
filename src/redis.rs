d3311406e327c6674c94ffb4e6b0c82f478c4f74
Max Strübing <mxstrbng@gmail.com>
2025-03-09 05:24:00 +0000

    feat: proper connection pooling

--- src/redis.rs
+++ src/redis.rs
@@ -1,28 +1,39 @@
-use redis::Commands;
+use r2d2::Pool;
+use r2d2_redis::redis::{parse_redis_url, Commands};
+use r2d2_redis::RedisConnectionManager;
 use std::env;
 
-fn connect() -> redis::Connection {
-    //format - host:port
-    let redis_url = env::var("REDIS_URL").expect("missing environment variable REDIS_URL");
+type RedisPool = Pool<RedisConnectionManager>;
 
-    redis::Client::open(redis_url)
-        .expect("Invalid connection URL")
-        .get_connection()
-        .expect("failed to connect to Redis")
+fn create_pool() -> RedisPool {
+    let redis_url = env::var("REDIS_URL").expect("missing environment variable REDIS_URL");
+    let manager = RedisConnectionManager::new(parse_redis_url(&redis_url).unwrap())
+        .expect("Invalid connection URL");
+
+    Pool::builder()
+        .build(manager)
+        .expect("Failed to create pool")
+}
+
+lazy_static::lazy_static! {
+    static ref POOL: RedisPool = create_pool();
 }
 
 pub fn set(key: &str, value: &str) {
-    let mut con = connect();
+    let pool = POOL.clone();
+    let con = pool.get().expect("Failed to get connection from pool");
     let _: () = con.set(key, value).expect("failed to set key");
 }
 
 pub fn get(key: &str) -> Option<String> {
-    let mut con = connect();
+    let pool = POOL.clone();
+    let con = pool.get().expect("Failed to get connection from pool");
     let value: Option<String> = con.get(key).expect("failed to get key");
     value
 }
 
-pub fn expire(key: &str, seconds: i64) {
-    let mut con = connect();
+pub fn expire(key: &str, seconds: usize) {
+    let pool = POOL.clone();
+    let con = pool.get().expect("Failed to get connection from pool");
     let _: () = con.expire(key, seconds).expect("failed to set expiry");
 }
