d1e4d18992284fb3f9a3ba35b5adf2bf8465e0ac
Max Strübing <mxstrbng@gmail.com>
2025-03-09 14:28:35 +0000

    refactor: use profile or custom id as cache key

--- src/steam.rs
+++ src/steam.rs
@@ -1,6 +1,7 @@
 use regex::Regex;
+use reqwest::Url;
 
-pub fn get_id(lines: &str) -> Option<String> {
+pub fn get_steam_id_from_html(lines: &str) -> Option<String> {
     let target_line = lines.lines().find(|line| line.contains("steamid"));
     let re = Regex::new(r#"^.*"steamid":"(\d*)".*$"#).unwrap();
     let Some(caps) = re.captures(target_line?) else {
@@ -9,4 +10,18 @@ pub fn get_id(lines: &str) -> Option<String> {
 
     let steam_id = &caps[1];
     Some(steam_id.to_string())
+}
+
+pub fn get_id_from_url(url: &str) -> Option<String> {
+    let parsed_url = Url::parse(url).unwrap();
+
+    let ab = parsed_url
+        .path_segments()?
+        .enumerate()
+        .find(|(i, _segment)| *i == 1);
+
+    match ab {
+        Some((_, segment)) => Some(segment.to_string()),
+        None => None,
+    }
 }
