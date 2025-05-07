89f576eea799a33eff688d9c5620647936233503
Max Strübing <mxstrbng@gmail.com>
2025-05-07 11:13:08 +0000

    feat: make extension more beautiful

---
+++ extension/src/API.elm
@@ -0,0 +1,64 @@
+module API exposing (Response, Site, assetUrl, baseUrl, playerUrl, responseDecoder, responseEncoder, siteDecoder, siteEncoder, sitesDecoder)
+
+import Json.Decode exposing (Decoder, field)
+import Json.Encode
+
+
+type alias Site =
+    { title : String, url : String }
+
+
+type alias Response =
+    { steam_id : String
+    , sites : List Site
+    }
+
+
+baseUrl : String
+baseUrl =
+    "https://finder.maex.me"
+
+
+playerUrl : String
+playerUrl =
+    baseUrl ++ "/player"
+
+
+assetUrl : String
+assetUrl =
+    baseUrl ++ "/assets"
+
+
+responseDecoder : Decoder Response
+responseDecoder =
+    Json.Decode.map2 Response
+        (field "steam_id" Json.Decode.string)
+        sitesDecoder
+
+
+sitesDecoder : Decoder (List Site)
+sitesDecoder =
+    field "sites" (Json.Decode.list siteDecoder)
+
+
+siteDecoder : Decoder Site
+siteDecoder =
+    Json.Decode.map2 Site
+        (field "title" Json.Decode.string)
+        (field "url" Json.Decode.string)
+
+
+responseEncoder : Response -> Json.Encode.Value
+responseEncoder response =
+    Json.Encode.object
+        [ ( "steam_id", Json.Encode.string response.steam_id )
+        , ( "sites", Json.Encode.list siteEncoder response.sites )
+        ]
+
+
+siteEncoder : Site -> Json.Encode.Value
+siteEncoder site =
+    Json.Encode.object
+        [ ( "title", Json.Encode.string site.title )
+        , ( "url", Json.Encode.string site.url )
+        ]
---
+++ extension/src/Components.elm
@@ -0,0 +1,114 @@
+module Components exposing (IconType(..), icon, link, logo, sideBar, stringToIconType)
+
+import API exposing (assetUrl)
+import Html exposing (Html)
+import Html.Attributes exposing (style)
+
+
+logo : Html msg
+logo =
+    icon (Just ProfilePeek) 40 40
+
+
+column : List (Html msg) -> Html msg
+column children =
+    Html.div
+        [ style "display" "flex"
+        , style "flex-direction" "column"
+        , style "align-items" "center"
+        , style "justify-content" "center"
+        , style "width" "100%"
+        , style "height" "100%"
+        , style "gap" "15px"
+        ]
+        children
+
+
+sideBar : List (Html msg) -> Html msg
+sideBar children =
+    Html.div
+        [ style "position" "fixed"
+        , style "top" "50%"
+        , style "transorms" "translateY(-50%)"
+        , style "left" "10px"
+        , style "background-color" "black"
+        , style "padding" "4px"
+        , style "border-radius" "100px"
+        , style "border" "1px solid grey"
+        ]
+        [ column children ]
+
+
+link : String -> Html msg -> Html msg
+link url children =
+    Html.a
+        [ Html.Attributes.href url
+        , style "display" "inline-flex"
+        , style "align-items" "center"
+        , style "justify-content" "center"
+        , style "padding" "0"
+        , style "margin" "0"
+        , style "text-decoration" "none"
+        , style "line-height" "1"
+        , Html.Attributes.target "_blank"
+        , Html.Attributes.rel "noopener noreferrer"
+        ]
+        [ children ]
+
+
+type IconType
+    = Leetify
+    | CsStats
+    | ProfilePeek
+
+
+icon : Maybe IconType -> Int -> Int -> Html msg
+icon maybeIconType width heigth =
+    case maybeIconType of
+        Just iconType ->
+            Html.img [ Html.Attributes.title (iconTypeToString iconType), Html.Attributes.src (iconMapping iconType), Html.Attributes.width width, Html.Attributes.height heigth ] []
+
+        Nothing ->
+            Html.text ""
+
+
+iconMapping : IconType -> String
+iconMapping iconType =
+    case iconType of
+        Leetify ->
+            assetUrl ++ "/icons/leetify.svg"
+
+        CsStats ->
+            assetUrl ++ "/icons/csstats.svg"
+
+        ProfilePeek ->
+            assetUrl ++ "/icons/logo-button.svg"
+
+
+stringToIconType : String -> Maybe IconType
+stringToIconType str =
+    case str of
+        "Leetify" ->
+            Just Leetify
+
+        "csstats" ->
+            Just CsStats
+
+        "ProfilePeek" ->
+            Just ProfilePeek
+
+        _ ->
+            Nothing
+
+
+iconTypeToString : IconType -> String
+iconTypeToString iconType =
+    case iconType of
+        Leetify ->
+            "Leetify"
+
+        CsStats ->
+            "csstats"
+
+        ProfilePeek ->
+            "ProfilePeek"
